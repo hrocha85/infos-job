1f544fde151d46cf08db132cfddb2d5e38179516
Henrique dos Reis Rocha <hrocha85@gmail.com>
2024-10-02 12:24:02 -0300

    fix axios

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,11 +1,13 @@
-export const fetchTopHeadlines = async () => {
-  const response = await fetch(
-    "https://newsapi.org/v2/top-headlines?country=us&apiKey=6b49c2d0c90f41dcb1754e4c440861ff"
-  );
+import axios from "axios";
 
-  if (!response.ok) {
+export const fetchTopHeadlines = async () => {
+  try {
+    const response = await axios.get(
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=6b49c2d0c90f41dcb1754e4c440861ff"
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
     throw new Error("Network response was not ok");
   }
-
-  return response.json();
 };
